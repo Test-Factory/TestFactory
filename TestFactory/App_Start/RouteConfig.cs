e16001f52a7912365681f5c92c1476305c7a19c2
Vlad Stepchin <vladmasterman@gmail.com>
2015-09-14 17:32:28 +0300

    added views, controllers and some architecture

--- TestFactory/App_Start/RouteConfig.cs
+++ TestFactory/App_Start/RouteConfig.cs
@@ -14,10 +14,34 @@ namespace TestFactory
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                   name: "Default",
+                   url: "",
+                   defaults: new { controller = "Home", action = "Index" }
+                   );
+
+            routes.MapRoute(
+               name: "studentsList",
+               url: "user/students/{id}",
+               defaults: new { controller = "User", action = "Students", id = UrlParameter.Optional }
+           );
+
+            routes.MapRoute(
+               name: "groupsList",
+               url: "groupList",
+               defaults: new { controller = "Home", action = "Index" }
+           );
+
+            routes.MapRoute(
+               name: "login",
+               url: "user/logIn",
+               defaults: new { controller = "User", action = "LogIn" }
+           );
+
+            routes.MapRoute(
+               name: "results",
+               url: "user/results/{id}",
+               defaults: new { controller = "User", action = "Results", id = UrlParameter.Optional }
+           );
         }
     }
 }
---
+++ TestFactory/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestFactory.Controllers
+{
+    public class UserController : Controller
+    {
+        //
+        // GET: /User/
+
+        public ActionResult Students()
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        public ActionResult Results()
+        {
+            return View();
+        }
+    }
+}
