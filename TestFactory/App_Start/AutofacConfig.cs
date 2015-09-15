376017c25c4e7c5e333176842aac6e7d0dcaa374
olenka.pokotilo <olenka.pokotilo@gmail.com>
2015-09-15 23:02:49 +0300

    add autofac, autofaConfig and register it in global.acax

---
+++ TestFactory/App_Start/AutofacConfig.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Autofac;
+using Autofac.Integration.Mvc;
+using NHibernateDataProviders.Data_Providers;
+using TestFactory.Business.Data_Provider_Contracts;
+using TestFactory.NHibernateDataProvider.Data_Providers;
+
+namespace TestFactory.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void RegisterDependencies()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(Assembly.GetExecutingAssembly());
+
+            builder.RegisterType<NHibernateUserDataProvider>()
+                .As<IUserDataProvider>();
+
+            builder.RegisterType<NHibernateGroupDataProvider>()
+                .As<IGroupDataProvider>();
+
+            builder.RegisterType<NHibernateStudentDataProvider>()
+                .As<IStudentDataProvider>();
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
