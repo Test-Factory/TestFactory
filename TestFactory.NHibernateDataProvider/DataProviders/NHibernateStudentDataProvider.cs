28ab6c48048ba5cdc845ebc02f3205ef319839ac
olenka.pokotilo <olenka.pokotilo@gmail.com>
2015-09-21 08:42:55 +0300

    Fix GetByGroupId - use linq

--- TestFactory.NHibernateDataProvider/DataProviders/NHibernateStudentDataProvider.cs
+++ TestFactory.NHibernateDataProvider/DataProviders/NHibernateStudentDataProvider.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using NHibernate;
 using NHibernate.Criterion;
+using NHibernate.Linq;
+using NHibernateDataProviders.NHibernateCore;
 using TestFactory.Business.DataProviderContracts;
 using TestFactory.Business.Models;
 
@@ -10,10 +14,9 @@ namespace TestFactory.NHibernateDataProvider.DataProviders
     {
         public IList<Student> GetByGroupId(string groupId)
         {
-            return Execute(session => session
-                .CreateCriteria<Student>()
-                .Add(Restrictions.Eq("GroupId", groupId))
-                .List<Student>()); 
+            return Execute(session => session.Query<Student>()
+                .Where(s => s.Group.Id == groupId)
+                .ToList<Student>()); 
         }
     }
 }
