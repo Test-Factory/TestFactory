4b92930398fa51a5338a9c2d34aba11192d0ac19
mrrbodia <02bodia20@ukr.net>
2015-09-22 10:39:35 +0300

    add MarkManager and providers

---
+++ TestFactory.NHibernateDataProvider/DataProviders/NHibernateMarkDataProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NHibernate;
+using NHibernate.Criterion;
+using NHibernate.Linq;
+using NHibernateDataProviders.NHibernateCore;
+using TestFactory.Business.DataProviderContracts;
+using TestFactory.Business.Models;
+
+namespace TestFactory.NHibernateDataProvider.DataProviders
+{
+    public class NHibernateMarkDataProvider : NHibernateDataProviderBase<Mark>, IMarkDataProvider
+    {
+        public IList<Mark> GetByStudentId(string studentId)
+        {
+            return Execute(session => session.Query<Mark>()
+                .Where(s => s.Students.Id == studentId)
+                .ToList<Mark>());
+        }
+    }
+}
