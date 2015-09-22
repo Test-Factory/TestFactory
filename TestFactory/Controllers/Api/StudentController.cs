74952c4a55475970200ee999106706ca2f97d3a7
olenka.pokotilo <olenka.pokotilo@gmail.com>
2015-09-22 22:51:05 +0300

    add web api StudentsController

---
+++ TestFactory/Controllers/Api/StudentController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Messaging;
+using System.Web.Http;
+using TestFactory.Business.Models;
+
+namespace TestFactory.Controllers.Api
+{
+    [RoutePrefix("api/students")]
+    public class StudentsController : ApiController
+    {
+        // GET: API/Student
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return new List<Student>();
+        }
+       
+        [HttpPost]
+        public IHttpActionResult Create()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IHttpActionResult Update()
+        {
+            return Ok();
+        }
+    }
+}
