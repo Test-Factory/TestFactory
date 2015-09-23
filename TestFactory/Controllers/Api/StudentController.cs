e3cfcfadc89ace0e18f69bfc8c039e96195b9ea4
olenka.pokotilo <olenka.pokotilo@gmail.com>
2015-09-23 12:11:38 +0300

    register api controllers, autofac webapi2

--- TestFactory/Controllers/Api/StudentController.cs
+++ TestFactory/Controllers/Api/StudentController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Runtime.Remoting.Messaging;
 using System.Web.Http;
+using TestFactory.Business.Components.Managers;
 using TestFactory.Business.Models;
 
 namespace TestFactory.Controllers.Api
@@ -9,10 +10,18 @@ namespace TestFactory.Controllers.Api
     [RoutePrefix("api/students")]
     public class StudentsController : ApiController
     {
+        private readonly StudentManager studentManager;
+
+        public StudentsController(StudentManager studentManager)
+        {
+            this.studentManager = studentManager;
+        }
         // GET: API/Student
         [HttpGet]
         public IEnumerable<Student> Get()
         {
+            IEnumerable<Student> students = studentManager.GetList();
+            
             return new List<Student>();
         }
        
