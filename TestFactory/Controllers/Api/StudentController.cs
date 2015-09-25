65a09c7dc9907379071412eded64b6150e780ce7
olenka.pokotilo <olenka.pokotilo@gmail.com>
2015-09-25 10:52:30 +0300

    add category and group providers add category api contr

--- TestFactory/Controllers/Api/StudentController.cs
+++ TestFactory/Controllers/Api/StudentController.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Runtime.Remoting.Messaging;
 using System.Web.Http;
 using System.Web.Optimization;
+using System.Web.Routing;
+using AutoMapper;
 using TestFactory.Business.Components.Managers;
 using TestFactory.Business.Models;
 using System.Web;
 using System.Web.Providers.Entities;
+using TestFactory.MVC.ViewModels;
 
 namespace TestFactory.Controllers.Api
 {
@@ -24,10 +27,10 @@ namespace TestFactory.Controllers.Api
         }
         // GET: API/Student
         [HttpGet]
-        public IEnumerable<Student> Get()
+        public IEnumerable<StudentViewModel> Get()
         {
             IList<Student> students;
-            string groupId = "13b66a40-5b78-48a0-b209-1390e420a11e";
+            //string groupId = "13b66a40-5b78-48a0-b209-1390e420a11e";
             if (string.IsNullOrEmpty(groupId))
             {
                 students = studentManager.GetList();
@@ -40,14 +43,29 @@ namespace TestFactory.Controllers.Api
                     stud.Marks = markManager.GetList(stud.Id);
                 }*/
                 //checking role
-
             }
-            return students ;
+            var result = Mapper.Map<IEnumerable<StudentViewModel>>(students);
+            return result;
         }
        
         [HttpPost]
         public IHttpActionResult Create(Student student)
         {
+            var model = Mapper.Map<Student>(student);
+            // TODO: take from model
+            string groupId = RouteData.Values["groupId"].ToString();
+            model.GroupId = groupId;
+            studentManager.Create(model);
+            IList<Category> tDesc = categoryManager.GetList();
+            var i = 0;
+            foreach (Mark mr in model.Marks)
+            {
+                mr.Category = tDesc[i];
+                mr.StudentId = model.Id;
+                markManager.Create(mr);
+                i++;
+            }
+            return
             return Ok(1);
         }
         [HttpPut]
---
+++ TestFactory/Scripts/app/dataProvider/groupProvider.js
@@ -0,0 +1,27 @@
+﻿function GroupProvider() { }
+
+$(function () {
+    var path = settings.basePath + "/api/groups";
+    var contentType = "application/json; charset=utf-8";
+    StudentProvider.prototype.get = function (callback) {
+        $.get(path).done(callback).error(function () { console.log("error"); });
+    }
+
+    StudentProvider.prototype.post = function (data, callback) {
+        $.ajax({
+            method: "POST",
+            url: path,
+            data: JSON.stringify(data),
+            contentType: contentType,
+        }).done(callback);
+    }
+
+    StudentProvider.prototype.put = function (data, callback) {
+        $.ajax({
+            method: "PUT",
+            url: path,
+            data: JSON.stringify(data),
+            contentType: contentType,
+        }).done(callback);
+    }
+});
