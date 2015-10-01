d19976c18a6cc31e576b344493658e0a0dfbde85
olenka.pokotilo <olenka.pokotilo@gmail.com>
2015-10-01 11:59:28 +0300

    remove resultContr add fileApiContr

--- TestFactory/Controllers/Api/FileController.cs
+++ TestFactory/Controllers/Api/FileController.cs
@@ -1,34 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using AutoMapper;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
+using AutoMapper;
 using TestFactory.Business.Components.Managers;
 using TestFactory.Business.Models;
 using TestFactory.MVC.ViewModels;
 
-namespace TestFactory.Controllers
+namespace TestFactory.Controllers.Api
 {
-    public class ResultController : Controller
+    public class FileController : ApiController
     {
         private readonly CategoryManager categoryManager;
-
         private readonly ResultManager resultManager;
 
-        public ResultController(CategoryManager categoryManager, ResultManager resultManager)
+        public FileController(CategoryManager categoryManager, ResultManager resultManager)
         {
             this.categoryManager = categoryManager;
             this.resultManager = resultManager;
         }
 
-        public bool Result(StudentViewModel student = null)
-        {
+        [HttpGet]
+        public bool GetReport(StudentViewModel student)
+        {  
             var  studentSave = Mapper.Map<Student>(student);
             IList<Category> categories = categoryManager.GetList();
             resultManager.SaveToWord(studentSave, categories);
             return true;
+
         }
     }
 }
