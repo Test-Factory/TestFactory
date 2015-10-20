d9ff13e9a09c8c3bdcb356689b208f4670e2dbea
mrrbodia <02bodia20@ukr.net>
2015-10-20 13:51:18 +0300

    add database scripts

---
+++ SolutionItems/SqlScripts/DataBase/dbo.Role.data.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO [dbo].[Role] ([Id], [Name]) VALUES (N'12dc6a23-8454-419f-ac75-2ea0560d27ef', N'Filler')
+INSERT INTO [dbo].[Role] ([Id], [Name]) VALUES (N'316987d9-9e4e-4cc4-b32a-b64112ca20be', N'Editor')
