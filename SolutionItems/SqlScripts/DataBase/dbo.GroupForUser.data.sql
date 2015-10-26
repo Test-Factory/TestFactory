41dbabda8a6d60a7b7aaf453af169032dba6fa77
mrrbodia <02bodia20@ukr.net>
2015-10-26 20:32:59 +0200

    backup database

---
+++ SolutionItems/SqlScripts/DataBase/dbo.GroupForUser.data.sql
@@ -0,0 +1,4 @@
+﻿INSERT INTO [dbo].[GroupForUser] ([Id], [GroupId], [UserId]) VALUES (N'0ec79ba1-f5ee-4e7d-b8d2-908b9a1a35d1', N'5de0fbe3-9301-465f-abe7-fa46042dbd55', N'887acb6d-2ba6-4323-8d7f-826bb78fd455')
+INSERT INTO [dbo].[GroupForUser] ([Id], [GroupId], [UserId]) VALUES (N'8cdda624-d1bf-48d1-b0c9-fb6e047c9c36', N'42548ebc-2407-484f-822e-9dc487f925f9', N'887acb6d-2ba6-4323-8d7f-826bb78fd455')
+INSERT INTO [dbo].[GroupForUser] ([Id], [GroupId], [UserId]) VALUES (N'92c66111-325f-4dbe-a5ec-4e2bb8f90a81', N'022483b5-32de-454b-a6c7-41c0b213e82f', N'887acb6d-2ba6-4323-8d7f-826bb78fd455')
+INSERT INTO [dbo].[GroupForUser] ([Id], [GroupId], [UserId]) VALUES (N'cdd0e4f2-5d3f-4649-8493-26d531b4f4db', N'1cb8a5d5-e644-48f8-b8b6-ee0c3cf4700f', N'887acb6d-2ba6-4323-8d7f-826bb78fd455')
