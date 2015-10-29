e9d793ee1bc55ca32e063d571ba352810b0513da
olenka.pokotilo <olenka.pokotilo@gmail.com>
2015-10-29 16:03:07 +0200

    add new student for all model.js and new viewModel studentsAll add new routing

---
+++ TestFactory/Scripts/app/models/studentForAllModel.js
@@ -0,0 +1,23 @@
+﻿function StudentForAllModel(item) {
+    var self = this;
+    var groupProvider = new GroupProvider();
+
+    self.id = ko.observable(item ? item.Id : "");
+    self.firstName = ko.observable(item ? item.FirstName : "");
+    self.lastName = ko.observable(item ? item.LastName : "");
+    self.groupId = ko.observable(item ? item.GroupId : "");
+
+    self.group = ko.observable();
+    groupProvider.get(self.groupId, function(data) {
+        var mappedItem = new GroupModel(data);
+        self.group(mappedItem);
+    });
+
+    self.marks = ko.observableArray();
+    if (item && item.Marks) {
+        item.Marks.forEach(function (element) {
+            var mappedItem = new MarkModel(element);
+            self.marks.push(mappedItem);
+        });
+    }
+}
---
+++ TestFactory/Scripts/app/viewModels/studentsAll.js
@@ -0,0 +1,115 @@
+﻿function  StudentsAllViewModel(parameters) {
+    var self = this;
+
+    var studentProvider = new StudentProvider();
+    var categoryProvider = new CategoryProvider();
+
+    self.students = ko.observableArray();
+    self.categories = ko.observableArray();
+
+    self.sortDescending = ko.observable(false);
+    self.sortKey = ko.observable("lastName");
+
+    self.sortingByMark = function (key, id, code) {
+        self.sortKey(code());
+        if (self.sortDescending()) {
+            self.students.sort(function (left, right) {
+
+                var getMark = function (item) {
+                    return item.categoryId() == id();
+                }
+                var leftMark = ko.utils.arrayFilter(left.marks(), getMark)[0];
+                var rightMark = ko.utils.arrayFilter(right.marks(), getMark)[0];
+
+                if (leftMark.value() == rightMark.value())
+                    return 0;
+                else if (leftMark.value() < rightMark.value())
+                    return 1;
+                else
+                    return -1;
+            });
+            self.sortDescending(false);
+        } else {
+            self.students.sort(function (left, right) {
+
+                var getMark = function (item) {
+                    return item.categoryId() == id();
+                }
+                var leftMark = ko.utils.arrayFilter(left.marks(), getMark)[0];
+                var rightMark = ko.utils.arrayFilter(right.marks(), getMark)[0];
+
+                if (leftMark.value() == rightMark.value())
+                    return 0;
+                else if (leftMark.value() < rightMark.value())
+                    return -1;
+                else
+                    return 1;
+            });
+            self.sortDescending(true);
+        }
+
+    }
+
+    self.sortingByName = function (key) {
+        self.sortKey(key);
+        if (self.sortDescending()) {
+            self.students.sort(function (left, right) {
+
+                if (left[key]().toUpperCase() == right[key]().toUpperCase())
+                    return 0;
+                else if (left[key]().toUpperCase() < right[key]().toUpperCase())
+                    return 1;
+                else
+                    return -1;
+            });
+            self.sortDescending(false);
+        } else {
+            self.students.sort(function (left, right) {
+                if (left[key]().toUpperCase() == right[key]().toUpperCase())
+                    return 0;
+                else if (left[key]().toUpperCase() < right[key]().toUpperCase())
+                    return -1;
+                else
+                    return 1;
+            });
+            self.sortDescending(true);
+        }
+
+    }
+
+    self.selectedClassForSortedField = ko.pureComputed(function () {
+        return self.sortDescending() ? "triangle-up" : "triangle-down";
+    }, self);
+
+    self.init = function () {
+        categoryProvider.get(function (data) {
+            $(data).each(function (index, element) {
+                var mappedItem = new CategoryModel(element);
+                self.categories.push(mappedItem);
+            });
+        });
+
+        studentProvider.get(function (data) {
+            $(data).each(function (index, element) {
+                var mappedStudent = new StudentForAllModel(element);
+                sortStudentMarksByCategoryIdDesc(mappedStudent);
+                self.students.push(mappedStudent);
+            });
+
+        });
+        self.sortingByName("lastName");
+    }
+
+    self.init();
+
+    function sortStudentMarksByCategoryIdDesc(student) {
+        student.marks.sort(function (leftMark, rightMark) {
+            if (leftMark.categoryId() == rightMark.categoryId())
+                return 0;
+            else if (leftMark.categoryId() < rightMark.categoryId())
+                return -1;
+            else
+                return 1;
+        });
+    }
+}
