f1a26eeb9bbfdbe6fb8908c99b838f7615cfcbd2
VladStepchin <vladmasterman@gmail.com>
2015-10-29 22:11:35 +0200

    addded some script for base backup

---
+++ TestFactory/dbo.VersionInfo.data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[VersionInfo] ([Version], [AppliedOn], [Description]) VALUES (201510220252, N'2015-10-28 18:14:06', N'M001_Initial')
+INSERT INTO [dbo].[VersionInfo] ([Version], [AppliedOn], [Description]) VALUES (201510250916, N'2015-10-28 18:14:06', N'M002_DefaultData')
+INSERT INTO [dbo].[VersionInfo] ([Version], [AppliedOn], [Description]) VALUES (201510261139, N'2015-10-28 18:14:10', N'M003_BackUpLiveDb')
