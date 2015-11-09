664da92eb10a66a73168ab7c0d7c930b487b81ec
olenka.pokotilo <olenka.pokotilo@gmail.com>
2015-11-09 11:27:34 +0200

    create model in business, create view model in mvc and add nhibernate mapping  for FrequencyMarkForFacultyByCategory

---
+++ TestFactory.MVC/ViewModels/FrequencyMarkForFacultyByCategoryViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFactory.MVC.ViewModels
+{
+    public class FrequencyMarkForFacultyByCategoryViewModel
+    {
+        public virtual string Faculty { get; set; }
+        public virtual string Code { get; set; }
+        public virtual string Id { get; set; }
+        public virtual float Count { get; set; }
+    }
+}
---
+++ TestFactory.NHibernateDataProvider/NHibernateCore/Mappings/FrequencyMarkForFacultyByCategoryMap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using TestFactory.Business.Models;
+
+namespace TestFactory.NHibernateDataProvider.NHibernateCore.Mappings
+{
+    public class FrequencyMarkForFacultyByCategoryMap: ClassMap<FrequencyMarkForFacultyByCategory>
+     {
+        public FrequencyMarkForFacultyByCategoryMap()
+        {
+            Map(x => x.Faculty);
+
+            Map(x => x.Code);
+
+            Id(x => x.Id);
+
+            Map(x => x.Count);
+        }
+    }
+}
