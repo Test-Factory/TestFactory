add0da4034c2b5c41dd8ebd47371f5daeaf00d71
olenka.pokotilo <olenka.pokotilo@gmail.com>
2015-11-10 11:09:19 +0200

    add model Subject in business, nhibernate map for subject, move folder with file.sql

---
+++ TestFactory.Business/Models/Subject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFactory.Business.Models
+{
+    public class Subject  : BaseModel
+    {
+        public virtual string Name { get; set; }
+        public virtual string GroupId { get; set; }
+    }
+}
---
+++ TestFactory.NHibernateDataProvider/NHibernateCore/Mappings/SubjectMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using NHibernate.Mapping;
+using TestFactory.Business.Models;
+
+namespace TestFactory.NHibernateDataProvider.NHibernateCore.Mappings
+{
+    public class SubjectMap  : ClassMap<Subject>
+    {
+        public SubjectMap()
+        {
+            Id(x => x.Id);
+            Map(x => x.Name);
+            Map(x => x.GroupId);
+        }
+    }
+}
