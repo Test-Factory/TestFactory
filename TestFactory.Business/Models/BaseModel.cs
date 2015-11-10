ee2a646d66c3d3ec96421fc0eefc12a842f1cc2a
mrrbodia <02bodia20@ukr.net>
2015-11-10 23:26:48 +0200

    add HiddenInput attribute for id

--- TestFactory.Business/Models/BaseModel.cs
+++ TestFactory.Business/Models/BaseModel.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using TestFactory.Business.Components.Lucene.Attributes;
 using Lucene.Net.Documents;
+using System.Web.Mvc;
 
 namespace TestFactory.Business.Models
 {
     public class BaseModel
     {
         [Storable(Type = Field.Index.NOT_ANALYZED)]
+        [HiddenInput(DisplayValue = false)]
         public virtual string Id { get; set; }
 
         public BaseModel()
