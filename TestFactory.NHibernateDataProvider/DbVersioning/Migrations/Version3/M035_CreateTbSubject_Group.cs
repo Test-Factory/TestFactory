054d88f8bd09d7329e1bb671f12954d863542b0e
olenka.pokotilo <olenka.pokotilo@gmail.com>
2015-11-13 09:51:35 +0200

    new migrationn create table Subject_Group

---
+++ TestFactory.NHibernateDataProvider/DbVersioning/Migrations/Version3/M035_CreateTbSubject_Group.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentMigrator;
+
+namespace TestFactory.NHibernateDataProvider.DbVersioning.Migrations.Version3
+{
+    [Migration(201511130929)]
+    public class M035_CreateTbSubject_Group : Migration
+    {
+        public override void Up()
+        {
+            Execute.EmbeddedScript("CreateTbSubject_Group.sql");
+        }
+
+        public override void Down()
+        {
+            Execute.EmbeddedScript("DropTbSubject_Group.sql");
+        }
+    }
+}
---
+++ TestFactory.NHibernateDataProvider/DbVersioning/Scripts/Version3/DropTbSubject_Group.sql
@@ -0,0 +1,6 @@
+
+    ALTER TABLE[dbo].[Subject_Group] DROP CONSTRAINT [FK_Subject_Group_Group]
+
+    ALTER TABLE[dbo].[Subject_Group] DROP CONSTRAINT [FK_Subject_Group_Subject]      
+         
+    DROP TABLE [dbo].[Subject_Group]
