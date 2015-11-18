79e5ddc994c7aa876a7e65e8c3f024b0398c94b7
mrrbodia <02bodia20@ukr.net>
2015-11-18 11:18:03 +0200

    create SubjectMark script and fix json result for submit to server

---
+++ TestFactory/Scripts/SubjectMark.js
@@ -0,0 +1,13 @@
+﻿SubmitForm = function (elem, path) {
+    $(elem).parents(".form-mark").find(".preloader-visibility").show();
+    $(elem).parent(".form-mark").find(".icon-mark").hide();
+    var toServer = $(elem).parents(".form-mark").serialize();
+    $.post(
+        path,
+        toServer,
+        function (data) {
+            $(elem).parents(".form-mark").find(".preloader-visibility").hide();
+            $(elem).parent(".form-mark").find(".icon-mark").show();
+        }
+    )
+}
