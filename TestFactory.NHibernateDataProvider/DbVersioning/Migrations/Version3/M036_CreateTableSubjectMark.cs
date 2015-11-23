9dbbee085db80edfe648ce11c6084962cc8cf862
olenka.pokotilo <olenka.pokotilo@gmail.com>
2015-11-23 20:52:23 +0200

    fix migration m036

--- TestFactory.NHibernateDataProvider/DbVersioning/Migrations/Version3/M036_CreateTableSubjectMark.cs
+++ TestFactory.NHibernateDataProvider/DbVersioning/Migrations/Version3/M036_CreateTableSubjectMark.cs
@@ -8,7 +8,7 @@ using FluentMigrator;
 namespace TestFactory.NHibernateDataProvider.DbVersioning.Migrations.Version3
 {
     [Migration(201511170111)]
-    public class M032_CreateTableSubjectMark : Migration
+    public class M036_CreateTableSubjectMark : Migration
     {
         public override void Up()
         {
