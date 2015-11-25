acac541a68c23667b381372107622df9f04f651b
mrrbodia <02bodia20@ukr.net>
2015-11-26 01:08:07 +0200

    fix timer error

--- TestFactory/Scripts/SubjectMark.js
+++ TestFactory/Scripts/SubjectMark.js
@@ -1,13 +1,8 @@
 ﻿
-var timer = {}
-timer.Item = 0;
-timer.Item2 = 0;
-
 SubmitForm = function (elem, path) {
     $(elem).parents(".form-mark").find(".preloader-visibility").show();
     $(elem).parent(".form-mark").find(".icon-mark").hide();
     var toServer = $(elem).parents(".form-mark").serialize();
-    clearInterval(timer.Item)
     $.post(
         path,
         toServer,
@@ -16,7 +11,7 @@ SubmitForm = function (elem, path) {
                 Materialize.toast('Успішно відредаговано', 4000)
                 $(elem).parents(".form-mark").find(".preloader-visibility").hide();
                 $(elem).parent(".form-mark").find(".icon-mark-ok").show();
-                this.timer.Item = setTimeout(function () { $(elem).parent(".form-mark").find(".icon-mark-ok").hide(); }, 4000);
+                setTimeout(function () { $(elem).parent(".form-mark").find(".icon-mark-ok").hide(); }, 4000);
                 $(elem).removeClass("valid");
                 $(elem).removeClass("input-validation-error");
             }
@@ -24,10 +19,11 @@ SubmitForm = function (elem, path) {
                 Materialize.toast('Дані некоректні!', 4000)
                 $(elem).parents(".form-mark").find(".preloader-visibility").hide();
                 $(elem).parent(".form-mark").find(".icon-mark-bad").show();
-                this.timer.Item = setTimeout(function () { $(elem).parent(".form-mark").find(".icon-mark-bad").hide(); }, 4000);
+                alert(this.timer.Item);
+                setTimeout(function () { $(elem).parent(".form-mark").find(".icon-mark-bad").hide(); }, 4000);
                 $(elem).removeClass("valid");
                 $(elem).addClass("input-validation-error")
-                this.timer.Item2 = setTimeout(function () {$(elem).removeClass("input-validation-error")}, 4000);
+                setTimeout(function () {$(elem).removeClass("input-validation-error")}, 4000);
             }
         }
     )
