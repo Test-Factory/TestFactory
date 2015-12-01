f8e3101cc58e9f86ad8328de0476d2e98659ab45
olenka.pokotilo <olenka.pokotilo@gmail.com>
2015-12-01 11:12:30 +0200

    add empty mvc contr and api contr for user

---
+++ TestFactory/Controllers/Api/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TestFactory.Business.Components;
+using TestFactory.Filters;
+using TestFactory.MVC.ViewModels;
+
+namespace TestFactory.Controllers.Api
+{
+     [RoutePrefix("api/users")]
+    public class UsersController : ApiController
+    {
+         private readonly UserContext user;
+
+         public UsersController()
+        {
+            this.user = new UserContext();
+        }
+
+        [HttpGet]
+         public IList<UserViewModel> Get()
+        {
+            var result =  new List<UserViewModel>();
+            return result;
+        }
+
+        [HttpPost]
+        [ValidateModel]
+        public IHttpActionResult Create(UserViewModel user)
+        {
+            var model = new UserViewModel();
+            return Ok(model);
+        }
+
+        [HttpPut]
+        [ValidateModel]
+        public IHttpActionResult Update(UserViewModel user)
+        {
+            return Ok();
+        }
+
+    }
+}
--- TestFactory/Controllers/UserController.cs
+++ TestFactory/Controllers/UserController.cs
@@ -4,11 +4,20 @@ using TestFactory.Business.Components.Managers;
 using System.Web.Security;
 using Embedded_Resource;
 using RoleNames = TestFactory.Resources.RoleNames;
+using System.Collections.Generic;
 
 namespace TestFactory.Controllers
 {
     public class UserController : Controller
     {
+
+        [HttpGet]
+       // [Authorize(Roles = RoleNames.Admin)]
+        public ActionResult List()
+        {
+            var result = new List<UserViewModel>();
+            return View("List", result);
+        }
      
         [HttpGet]
         [AllowAnonymous]
